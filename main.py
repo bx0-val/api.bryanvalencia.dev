d388cd34607b64fa28fe3ae0f157d9c83da1f1e7
bx0-val <bryan@bryanvalencia.dev>
2025-07-22 16:11:22 -0500

    developing commits endpoint

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
 import xml.etree.ElementTree as ET
+import requests
+import os
+import json
 
 app = FastAPI()
 
@@ -23,7 +26,7 @@ app.add_middleware(
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/oplog", response_class=HTMLResponse)
-async def index(request: Request):
+async def oplog(request: Request):
     tree = ET.parse("oplog/oplog.xml")
     entries = tree.findall("entry")
     entries_list = []
@@ -32,4 +35,27 @@ async def index(request: Request):
 
     print(entries_list)
     entries_list.reverse()
-    return templates.TemplateResponse(name="index.html", context={'request': request, 'data': entries_list})
+    return templates.TemplateResponse(name="oplog.html", context={'request': request, 'data': entries_list})
+
+@app.get("/commits", response_class=HTMLResponse)
+async def commits(request: Request):
+    headers = {
+    'Accept': 'application/vnd.github+json',
+    'Authorization': f'Bearer {os.getenv('GITHUB_TOKEN')}',
+    'X-GitHub-Api-Version': '2022-11-28',
+    }
+
+    repos = requests.get('https://api.github.com/user/repos', headers=headers)
+    repos_list = [repo['name'] for repo in repos.json()] 
+    print(repos_list)
+
+    all_commits = {}
+    for repo in repos_list:
+        response = requests.get(f'https://api.github.com/repos/bx0-val/{repo}/commits', headers=headers)
+        commit_history = [(commit['html_url'], commit['sha'][:7],commit['commit']['author']['date'],commit['commit']['message']) for commit in response.json()]
+        all_commits[repo] = commit_history
+
+    # print(all_commits)
+    # with open("commits.json", "a") as f:
+    #     f.write(json.dumps(all_commits))
+    return templates.TemplateResponse(name="commits.html", context={'request': request, 'data': all_commits})
