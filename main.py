59cd46e97f4de572a9a69a4997dc62b9555c1cc3
bx0-val <bryan@bryanvalencia.dev>
2025-07-25 12:22:55 -0500

    modified /commits endpoint to utilize graphql endpoint, improving template hydration by 300%

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from fastapi.middleware.cors import CORSMiddleware
 import xml.etree.ElementTree as ET
 import requests
 import os
-import json
+import asyncio
+from timeit import default_timer
 
 app = FastAPI()
 
@@ -24,6 +25,10 @@ app.add_middleware(
 )
 
 templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse(name="index.html", context={'request': request})
 
 @app.get("/oplog", response_class=HTMLResponse)
 async def oplog(request: Request):
@@ -45,18 +50,18 @@ async def commits(request: Request):
     'Authorization': f'Bearer {token}',
     'X-GitHub-Api-Version': '2022-11-28',
     }
+    query={
+        "query": "query {\n  viewer {\n    repositories(first: 50, ownerAffiliations: OWNER) {\n      nodes {\n        name\n        url\n        history: defaultBranchRef {\n          target {\n            ... on Commit {\n              history(first: 10) {\n                nodes {\n                  oid\n                  messageHeadline\n                  committedDate\n                }\n              }\n            }\n          }\n        }\n      }\n    }\n  }\n}"
+        }
 
-    repos = requests.get('https://api.github.com/user/repos', headers=headers)
-    repos_list = [repo['name'] for repo in repos.json()] 
-    print(repos_list)
+    start = default_timer()
+    response = requests.post(f'https://api.github.com/graphql', headers=headers, json=query)
+    data=response.json()
+    print(data['data']['viewer']['repositories']['nodes'])
 
-    all_commits = {}
-    for repo in repos_list:
-        response = requests.get(f'https://api.github.com/repos/bx0-val/{repo}/commits', headers=headers)
-        commit_history = [(commit['html_url'], commit['sha'][:7],commit['commit']['author']['date'],commit['commit']['message']) for commit in response.json()]
-        all_commits[repo] = commit_history
+    # with open("graph.json", "w") as f:
+    #     f.write(response.text)
 
-    # print(all_commits)
-    # with open("commits.json", "a") as f:
-    #     f.write(json.dumps(all_commits))
-    return templates.TemplateResponse(name="commits.html", context={'request': request, 'data': all_commits})
+    return templates.TemplateResponse(name="commits.html", context={'request': request, 'data': data['data']['viewer']['repositories']['nodes']})
+
+# def request()
